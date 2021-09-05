ca6f46194947a4e816ce2c63aea8430ccfaecf02
107w <1319677040@qq.com>
2021-09-05 09:33:50 +0800

    push 2 commit

--- src/main/java/com/lv/git/GitTest.java
+++ src/main/java/com/lv/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("dwadwa");
         System.out.println("push test");
+        System.out.println("push 2");
     }
 }
