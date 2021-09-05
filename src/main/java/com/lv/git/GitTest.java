64d54089be888d34a0ee9b8dbc78b66214615be8
107w <1319677040@qq.com>
2021-09-05 08:29:44 +0800

    hot-fix test commit

--- src/main/java/com/lv/git/GitTest.java
+++ src/main/java/com/lv/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello lv");
         System.out.println("hello cq");
         System.out.println("hello hy by hot-fix");
+        System.out.println("hot-fix test");
     }
 }
